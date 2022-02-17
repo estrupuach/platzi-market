14ab8a33887f55825f7c36983a229e62b7db20b3
estrupuach <cristiancamilo0102@gmail.com>
2022-02-17 17:14:45 -0500

    Se crean las clases persistentes de las tablas de la base de datos

---
+++ src/main/java/com/platzi/market/persistence/entity/Cliente.java
@@ -0,0 +1,24 @@
+package com.platzi.market.persistence.entity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    private String id;
+
+    private String nombre;
+    private String apellidos;
+    private Double celular;
+    private String direccion;
+
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+
+}
